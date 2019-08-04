b1931f6c40cbee6ed46c8a21fdd7502bfc9254bf
nemanjan00 <nemanjan00@gmail.com>
2019-08-04 23:32:23 +0200

    Line cleanup

--- index.zsh
+++ index.zsh
@@ -4,5 +4,13 @@
 source ~/.zsh/functions.zsh
 
 # Plugin manager
+POWERLEVEL9K_MODE="nerdfont-complete"
+
 source ~/.zsh/zplug.zsh
+
+icons[LEFT_SEGMENT_SEPARATOR]=''
+icons[RIGHT_SEGMENT_SEPARATOR]=''
+icons[LEFT_SUBSEGMENT_SEPARATOR]=''
+icons[RIGHT_SUBSEGMENT_SEPARATOR]=''
+icons[TIME_ICON]=''
 
