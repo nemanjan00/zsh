d74d6f6cbefdc098729d414d6c2faf14198858f2
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2026-03-31 13:02:21 +0200

    Update aliases: clean up formatting and remove unused aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,13 +1,12 @@
-# Git
-
+# Nav
 alias ls="ls --color"
 alias k="exa -lah --git --icons"
 
+# Git
 alias add="git add"
 alias commit="git commit"
 #alias commit="git commit -S"
 alias push="git push"
-alias init="hub init"
 alias push-all="git remote | xargs -L1 git push"
 
 # Search
@@ -16,37 +15,24 @@ alias g="grep --line-buffered"
 alias ag="rg"
 
 # mpv audio only
-
 alias mpva="mpv --no-video"
 
 # web services
-
 alias wttr="curl https://wttr.in/Belgrade"
 
 # sync
-
 alias s="~/.i3/sync.sh"
 
 # fucking
-
 alias fucking="sudo"
 
 alias less="less -r"
 
 # IDE
-
 alias docker-dev='docker run -ti -eTERM=xterm-256color -v$(pwd):/work/project nemanjan00/dev zsh -ic "cd project ; tmux"'
 alias vim="nvim"
 alias v="nvim"
 
-# Firejail
-
-alias spotify="firejail spotify"
-
 # Cat
-
 alias ccat='pygmentize -g'
 
-
-# Linear tasks
-alias lt='lr list -m'
